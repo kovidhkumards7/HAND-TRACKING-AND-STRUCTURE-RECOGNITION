cf8f6401f66b557c5cfd1969248d27102ce8ec31
KOVIDH KUMAR D S <64972941+kovidhkumards7@users.noreply.github.com>
2024-01-25 21:14:11 +0530

    Add files via upload

---
+++ TrialTesting.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import time
+import mediapipe as mp
+
+# img = cv2.imread("28.jpg", 1)
+# print(img)
+# cv2.imshow("image", img)
+# cv2.waitKey(10000)
+# cv2.destryAllWindows()
+
+# img = cv.imread("28.jpg")
+# cv.imshow("28", img)
+# cv.waitKey(0)
+
+# capture = cv.VedioCapture("kk.mp4")
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow("Vedio", frame)
+#     if cv.waitKey(20) & 0xFF == ord("d"):
+#         break
+# capture.release()
+# cv.destroyAllWindows()
